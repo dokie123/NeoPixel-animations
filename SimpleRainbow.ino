23b416f8d9c0984313defd0776544af3ab09ea97
dokie123 <139399488+dokie123@users.noreply.github.com>
2024-04-30 14:27:37 -0500

    Update SimpleRainbow.ino

--- SimpleRainbow.ino
+++ SimpleRainbow.ino
@@ -1,1 +1,18 @@
+// there isnt much to explain
+// by iker fesser
+// last updated (M/D/Y): 4/30/2024
+#include <Adafruit_NeoPixel.h>
 
+// make sure to use specifications for you rings/strips/pixels
+Adafruit_NeoPixel Pixels(7, 3, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Pixels.begin();
+}
+
+void loop() {
+  for (int i = 0; i<Pixels.numPixels(); i++) {
+    Pixels.setPixelColor(i, Pixels.ColorHSV((65536/Pixels.numPixels())*i));
+  }
+  Pixels.show();
+}
